469fe7c69f93353d0654c43f40494d740bb18a3b
xuweiyu <1847449751@qq.com>
2020-04-07 23:35:23 +0800

    建造者模式源码

---
+++ app/src/main/java/com/xwy/designmode/mode/builder/sourcecode/BuilderTest.java
@@ -0,0 +1,14 @@
+package com.xwy.designmode.mode.builder.sourcecode;
+
+/**
+ * Created by xuweiyu on 2020-04-07.
+ * email: xuweiyu@igengmei.com
+ * 简介：
+ */
+public class BuilderTest {
+    public static void main(String[] args) {
+        StringBuilder builder = new StringBuilder();
+        builder.append("hello world");
+        System.out.println(builder);
+    }
+}
