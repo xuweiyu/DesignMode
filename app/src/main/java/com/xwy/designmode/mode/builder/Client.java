3e1096a739d4ae1cf1f4169afd9fa4cd3af6c195
xuweiyu <1847449751@qq.com>
2020-04-07 09:51:58 +0800

    建造者模式盖房子的常规实现

---
+++ app/src/main/java/com/xwy/designmode/mode/builder/Client.java
@@ -0,0 +1,13 @@
+package com.xwy.designmode.mode.builder;
+
+/**
+ * Created by xuweiyu on 2020-04-07.
+ * email: xuweiyu@igengmei.com
+ * 简介：
+ */
+public class Client {
+    public static void main(String[] args) {
+        House house = new HeighHouse();
+        house.build();
+    }
+}
