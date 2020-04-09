61aa47dabc97bb1b38dd1f494a9570fcbb4af922
xuweiyu <1847449751@qq.com>
2020-04-10 00:24:35 +0800

    装饰者模式源码

---
+++ app/src/main/java/com/xwy/designmode/mode/decorator/source/StreamTest.java
@@ -0,0 +1,20 @@
+package com.xwy.designmode.mode.decorator.source;
+
+import java.io.ByteArrayInputStream;
+import java.io.DataInputStream;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.InputStream;
+
+/**
+ * Created by xuweiyu on 2020-04-09.
+ * email: xuweiyu@igengmei.com
+ * 简介：
+ */
+public class StreamTest {
+    public static void main(String[] args) throws FileNotFoundException {
+        InputStream is = new FileInputStream(new File("xxx"));
+        DataInputStream dis = new DataInputStream(is);
+    }
+}
